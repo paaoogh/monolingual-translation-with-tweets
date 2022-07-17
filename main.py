84adfdfd287a40900018adacde21463d5ca21243
paaoogh <47153894+paaoogh@users.noreply.github.com>
2022-07-17 18:55:24 -0500

    Changing main

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import twint as tw
 import retrieve
 import read_info
 
-if __name__ == 'main':
-    print("hi")
-    gobernadoras, gobernadores = read_info.read_users('usuarios.csv')
+gobernadoras, gobernadores = read_info.read_users('usuarios.csv')
 
-    print("Starting with Gobernadoras")
-    data_gm = retrieve.replies_to_file(gobernadoras,"gobernadoras_small.csv")
-    print("Starting with Gobernadores")
-    data_gh = retrieve.replies_to_file(gobernadores,"gobernadores_small.csv")
+print("Starting with Gobernadoras")
+data_gm = retrieve.replies_to_file(gobernadoras,"gobernadoras_small.csv")
+print("Starting with Gobernadores")
+data_gh = retrieve.replies_to_file(gobernadores,"gobernadores_small.csv")
