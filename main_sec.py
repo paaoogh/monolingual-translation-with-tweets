2cc44b6d576390a01fe425db49fe8e947d905849
paaoogh <47153894+paaoogh@users.noreply.github.com>
2022-08-19 11:59:01 -0500

    Adding data secretarias

--- main_sec.py
+++ main_sec.py
@@ -7,7 +7,7 @@ import read_info
 import pandas as pd
 import twint as tw
 
-secretarias, secretarios = read_info.read_users('usuarios.csv')
+secretarias, secretarios = read_info.read_users('secretarixs_ejec.csv')
 print("Starting with Secretarias")
 data_gm = retrieve.replies_to_file(secretarias,"secretarias.csv")
 
